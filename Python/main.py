5694ead78af0451a595b89f1502b9933cfdc002e
Jungbin-Ahn <65687288+Jungbin-Ahn@users.noreply.github.com>
2022-09-09 01:08:14 -0400

    flask added

--- Python/main.py
+++ Python/main.py
@@ -1,3 +1,38 @@
-from file import save_to_file;
-from bs4 import BeautifulSoup;
+from flask import Flask, render_template, request, redirect, send_file
+from extractors.wwr import extract_wwr_jobs
+from file import save_to_file
 
+app = Flask("JobScrapper")
+
+db = {
+}
+
+
+"""it runs the funcion below"""
+@app.route("/") 
+def home():
+  return render_template("home.html", name = "nico")
+
+@app.route("/search") 
+def search():
+  keyword = request.args.get("keyword")
+  if keyword == None:
+    return redirect("/")
+  if keyword in db:
+    jobs = db[keyword]
+  else:  
+    wwr = extract_wwr_jobs(keyword)
+    jobs = wwr
+    db[keyword] = wwr
+  return render_template("search.html", keyword = keyword, jobs = jobs)
+
+@app.route("/export")
+def export():
+  keyword = request.args.get("keyword")
+  if keyword == None:
+    return redirect("/")
+  if keyword not in db:
+    return redirect(f"/search?keyword={keyword}")
+  save_to_file(keyword, db[keyword])
+  return send_file(f"{keyword}.csv", as_attachment = True)
+app.run("0.0.0.0")
